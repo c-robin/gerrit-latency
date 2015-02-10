2acd37fcde89252d3009a0c58b12fbc32c39a405
blayo <philippe.blayo@gmail.com>
2015-02-10 16:41:25 +0100

    Add config file. Use Requests lib instead of urllib
    Change-Id: I66bf7353842693a2e389f9da418f85389b7616f8

--- gerrit-latency.py
+++ gerrit-latency.py
@@ -5,6 +5,9 @@ import urllib
 import subprocess
 import re
 from urllib2 import Request, urlopen, URLError, HTTPBasicAuthHandler, HTTPPasswordMgrWithDefaultRealm, build_opener, install_opener
+import json
+from requests.auth import HTTPDigestAuth
+import requests
 
 class Commit():
 
@@ -15,26 +18,19 @@ class Commit():
 def parseDate(date_string):
     return datetime.strptime(date_string, "%d. %b %I:%M")
 
-gerrit_user = "crobin"
+config_file = open('passwd', 'r')
+gerrit_user = config_file.readline().rstrip()
+gerrit_pass = config_file.readline().rstrip()
+config_file.close()
+print("login: " + gerrit_user)
+print("passwd: " + gerrit_pass)
+
 gerrit_ssh_port = "29418"
 gerrit_http_port = "8080"
 gerrit_url = "localhost"
 ssh_public_key = "~/.ssh/id_rsa"
-gerrit_pass = ""
 #DO not forget to use status:merged in request
 gerrit_request = "owner:self status:merged"
-
-passman = HTTPPasswordMgrWithDefaultRealm()
-passman.add_password(None, gerrit_url + ":" + gerrit_http_port, gerrit_user, gerrit_pass)
-authhandler = HTTPBasicAuthHandler(passman)
-opener = build_opener(authhandler)
-install_opener(opener)
-
-# locale variable for strptime (windows)
-# locale.setlocale(locale.LC_ALL, 'american')
-# locale variable for strptime (Unix)
-# locale.setlocale(locale.LC_ALL, 'en_US')
-
 
 ret = subprocess.Popen(["ssh","-p",gerrit_ssh_port, "-i", ssh_public_key, gerrit_user + "@" + gerrit_url, "gerrit query " + gerrit_request], stdout=subprocess.PIPE);
 out, err = ret.communicate()
@@ -45,11 +41,12 @@ changes_list = list(set(re.findall("change (.{40})", out)))
 #Call gerrit_REST_API
 for change in changes_list:
     try:
-        request = Request("http://" + gerrit_url + ":8080/a/" + "changes/" + change + "/detail")
-        print "http://" + gerrit_url + ":" + gerrit_http_port + "/a/" + "changes/" + change + "/detail"
+        url = "http://" + gerrit_url + ":" + gerrit_http_port + "/a/" + "changes/" + change + "/detail"
+        print url
+        print "sending request"
+        r = requests.get(url, auth = HTTPDigestAuth(gerrit_user, gerrit_pass), timeout=None, proxies = None)
         print "request sent"
-        response = urlopen(request).read()
     
-        print response
+        print r.url
     except URLError, e:
         print 'REST error:', e
